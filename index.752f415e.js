238b7b7fe3a7608d2ba7a97272e0feb77b056eee
SergeyMark <110375004+SergeyMark@users.noreply.github.com>
2023-02-05 19:43:42 +0000

    Deploying to gh-pages from @ SergeyMark/filmoteka@138457e79f1cfa3382545a324babd2f578706ba9 🚀

--- index.752f415e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".movie-gallery");document.querySelector("body")&&fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=d03712107dcdd723f1173c5ee2c0d8c1").then((e=>{if(console.log(e),!e.ok)throw new Error(e.statusText);return e.json()})).then((e=>{console.log(e.results)}));
-//# sourceMappingURL=index.752f415e.js.map
