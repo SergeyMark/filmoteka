66c90d32e1636b64614f55086e261550bb7d5482
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-02-06 12:37:22 +0200

    add fuck team

--- src/js/searchMovie.js
+++ src/js/searchMovie.js
@@ -1,5 +1,6 @@
 import { featchMovie, featchOnSearchedMovie } from "./apiCreated";
 import { movieCreateCard } from "./movieCreateCard";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formSearcher = document.querySelector('.search-form');
 
@@ -9,8 +10,14 @@ function onSearchBtnClick(event){
     event.preventDefault();
 
     searchQuery = event.currentTarget.search.value.trim();
+
+    if (searchQuery === '') {
+        Notify.failure('please enter the name of the movie');
+    }
     
     featchOnSearchedMovie(searchQuery).then(response =>
+
         movieCreateCard(response.results)   
+    
     ).catch(error => console.log(error));
 };
